0d30d93ba6a79f06dddbb2992c27606f73b23608
Mohd Jaid <zaidkhan97932@gmail.com>
2026-07-25 11:08:05 +0530

    this is buble game

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let game_area = document.querySelector("#game-area");
+let m_point=0; //missed point
+let game_over = document.querySelector("#game-over");
+let t1=1000,t2=25;
+let letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const over = setInterval(CreateBubble,t1);
+// CreateBubble();
+setInterval(()=>{
+    if(m_point===15){
+        clearInterval(over);
+    }
+},t1);
+
+function CreateBubble(){
+let letter= letters[Math.floor(Math.random()*letters.length)]   ;
+let bubble= document.createElement("div");
+bubble.classList.add("bubbles"); //Class add
+game_area.appendChild(bubble);
+bubble.textContent= letter;
+let e_left = Math.random()*(window.innerWidth-70);
+bubble.style.left = e_left + "px"
+bubble.style.top = "-50px";
+let a=-50;
+const fall = setInterval(()=>{
+  
+    // console.log(a);
+    a+=3;
+      bubble.style.top=a+"px";
+    if(a > window.innerHeight){
+        bubble.remove();
+        clearInterval(fall);
+         m_point++;
+         document.getElementById("m-point").textContent=m_point;
+    }
+    if(m_point===15){
+        clearInterval(fall);
+         game_over.style.color="red";
+        game_over.textContent="Game over";
+    }
+},t2);
+   bubble.dataset.interval = fall;
+}
+
+let score=0; //game point
+document.addEventListener("keydown", (dets)=>{
+    const key = dets.key.toUpperCase();
+    let bubbles = document.querySelectorAll(".bubbles");
+    for(let bubble of bubbles){
+
+        if(bubble.innerText === key && m_point<15){
+          clearInterval(bubble.dataset.interval);
+          bubble.remove();
+          score += 5;
+          t2 -= 0.20;
+          document.getElementById("g-point").textContent=score;
+          break;
+        }
+    }
+})
